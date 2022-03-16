b8c99844a8111f32959259412c1e69fc65a833f3
ismail-khalil <ismail.k.aiub@gmail.com>
2022-03-17 04:19:05 +0600

    3rd code added

--- js/app.js
+++ js/app.js
@@ -16,4 +16,49 @@ $(document).ready(function(){
         console.log(percentage);
     })
 
+    //counter
+
+    const counters = document.querySelectorAll('.counter');
+    console.log(counters);
+
+    function runCounter () {
+        counters.forEach(counter => {
+            counter.innerText = 0;
+            let target = +counter.dataset.count;
+            let step = target / 100;
+
+            let countIt = function(){
+                let displayedCount = +counter.innerText;
+                if(displayedCount < target){
+                    counter.innerText = Math.ceil(displayedCount + step);
+                    setTimeout(countIt, 1);
+                } else {
+                    counter.innerText = target;
+                }
+            }
+            countIt();
+
+
+        })
+    }
+
+    //runCounter();
+    let counterSection = document.querySelector('.counter_section');
+
+    let options = {
+        rootMargin : '0px 0px -250px 0px'
+    }
+    let done = 0;
+
+    const sectionObserver = new IntersectionObserver(function(entries){
+
+        if(entries[0].isIntersecting && done !==1) {
+            done = 1;
+            runCounter();
+        }
+    }, options)
+
+
+    sectionObserver.observe(counterSection);
+
 });
